21b94a5f0893f1f09a52fb29f7d227db5727e90d
Adam Wright <awright18@gmail.com>
2023-09-06 19:42:39 -0500

    more optimizations

--- scripts/content-script.js
+++ scripts/content-script.js
@@ -1,6 +1,6 @@
 document.addEventListener("soft-nav:end", main);
 
-async function main () {
+async function main() {
 
     const openSavedRepliesButton = document.getElementById(
         "saved-reply-new_comment_field"
@@ -17,105 +17,62 @@ async function main () {
         return;
     }
 
-    const onRefreshRepliesButtonClick = async (event) => {
-
-        event.preventDefault();
-
-        const refreshImages = document.querySelectorAll(".particular-refresh-image");
-
-        if (refreshImages) {
-
-            for (let refreshImage of refreshImages) {
-                const refreshImageSrc = chrome.runtime.getURL("images/refresh-particular-replies-2.png");
-                refreshImage.src = refreshImageSrc;
-
-            }
-        }
-
-        const existingRepliesDivs = document.querySelectorAll(".particular-replies");
-
-        const savedReplies = await getSavedReplies(false);
-
-        const updatedRepliesDiv = createParticularRepliesDiv(savedReplies);
-
-        if (existingRepliesDivs) {
-
-            for (let repliesDiv of existingRepliesDivs) {
-                repliesDiv.replaceWith(updatedRepliesDiv);
-                console.log(`replaced!`)
-            }
-        }
-
-        if (refreshImages) {
-
-            for (let refreshImage of refreshImages) {
-                const refreshImageSrc = chrome.runtime.getURL("images/refresh-particular-replies.png");
-                refreshImage.src = refreshImageSrc;
-            }
-        }
-    }
-
-    const createParticularRepliesDivider = () => {
-
-        const refershImage = createElement("img", {
-            children: [],
-            className: "particular-refresh-image"
-        });
-
-        const refreshImageSrc = chrome.runtime.getURL("images/refresh-particular-replies.png");
-
-        refershImage.src = refreshImageSrc;
-
-        const refreshRepliesButton = createElement("button", {
-            children: [refershImage],
-            className: "refresh-particular-replies"
-        });
-
-        refreshRepliesButton.addEventListener(
-            "click",
-            onRefreshRepliesButtonClick
-        );
-
-        const repliesDivider = createElement("div", {
-            children: ["Particular Replies", refreshRepliesButton],
-            className: "select-menu-divider js-divider",
-        })
-
-        return repliesDivider;
-    }
-
     const isParticularGitHubIssueUrl = () => {
-         
-        let url = window.location.href;    
-        
+
+        let url = window.location.href;
+
         let pattern = /^(https?:\/\/)github\.com\/[Pp]articular\/[\dA-z\.-]+\/issues\/\d+$/g;
-        
+
         return pattern.test(url);
     }
+
+    const createSavedRepliesDiv = async () => {
+
+        const replies = await getSavedReplies();
+
+        const repliesDiv = createParticularRepliesDiv(replies)
+
+        return repliesDiv;
+    }
+
+    let repliesDiv = await createSavedRepliesDiv();
 
     const appendSavedReplies = async () => {
 
         const replyCategoriesDetailsMenus = document.querySelectorAll(
-            `markdown-toolbar details-menu[src^="/settings/replies?context="]`
+            `markdown-toolbar details-menu[src^="/settings/replies?context="] fuzzy-list div.select-menu-filters`// .select-menu-list
         );
 
-        const replies = await getSavedReplies();
-
-        const repliesDivider = createParticularRepliesDivider();
-
-        const repliesDiv = createParticularRepliesDiv(replies)
+        if (replyCategoriesDetailsMenus.length === 0) {
+            console.log("couldn't find spot!");
+        }
 
         for (const replyCategoriesDetailsMenu of replyCategoriesDetailsMenus) {
 
-            replyCategoriesDetailsMenu.appendChild(repliesDivider);
-
-            repliesDivider.insertAdjacentElement("afterend", repliesDiv);
+            replyCategoriesDetailsMenu.insertAdjacentElement("afterend", repliesDiv);
         }
     }
-   
-    if(isParticularGitHubIssueUrl()){
-        await appendSavedReplies();
+
+    // if (isParticularGitHubIssueUrl()) {
+    //     await appendSavedReplies();
+    // }
+
+    const onOpenSavedRepliesButtonClick = async () => {
+
+        if (isParticularGitHubIssueUrl()) {
+            await appendSavedReplies();
+        }
+
+        openSavedRepliesButton.removeEventListener(
+            "click",
+            onOpenSavedRepliesButtonClick
+        )
     }
+
+    openSavedRepliesButton.addEventListener(
+        "click",
+        onOpenSavedRepliesButtonClick
+    );
 }
 
 main().catch((error) => {
--- scripts/elements.js
+++ scripts/elements.js
@@ -22,14 +22,87 @@ const createElement = (tagName, { children, className, ...attributes } = {}) => 
     return element;
 }
 
-const createParticularRepliesDiv = (savedReplies) => {
+const onRefreshRepliesButtonClick = async (event) => {
 
-    const repliesDiv = createElement("div", {
+    event.preventDefault();
+
+    const refreshImages = document.querySelectorAll(".particular-refresh-image");
+
+    if (refreshImages) {
+
+        for (let refreshImage of refreshImages) {
+            const refreshImageSrc = chrome.runtime.getURL("images/refresh-particular-replies-2.png");
+            refreshImage.src = refreshImageSrc;
+
+        }
+    }
+
+    const existingRepliesDivs = document.querySelectorAll(".particular-replies");
+
+    const savedReplies = await getSavedReplies(false);
+
+    const updatedRepliesDiv = createParticularRepliesDiv(savedReplies);
+
+    if (existingRepliesDivs) {
+
+        for (let repliesDiv of existingRepliesDivs) {
+            repliesDiv.replaceWith(updatedRepliesDiv);              
+        }
+    }
+
+    if (refreshImages) {
+
+        for (let refreshImage of refreshImages) {
+            const refreshImageSrc = chrome.runtime.getURL("images/refresh-particular-replies.png");
+            refreshImage.src = refreshImageSrc;
+        }
+    }
+}
+
+const createParticularRepliesDivider = () => {
+
+    const refershImage = createElement("img", {
         children: [],
-        className: "particular-replies"
+        className: "particular-refresh-image"
     });
 
-    for (const reply of savedReplies) {
+    const refreshImageSrc = chrome.runtime.getURL("images/refresh-particular-replies.png");
+
+    refershImage.src = refreshImageSrc;
+
+    const refreshRepliesButton = createElement("button", {
+        children: [refershImage],
+        className: "refresh-particular-replies"
+    });
+
+    refreshRepliesButton.addEventListener(
+        "click",
+        onRefreshRepliesButtonClick
+    );
+
+    const repliesDivider = createElement("div", {
+        children: ["Particular Replies", refreshRepliesButton],
+        className: "select-menu-divider js-divider",
+    })
+
+    return repliesDivider;
+}
+
+const createParticularRepliesDiv = (savedReplies) => {
+
+    const repliesDivider = createParticularRepliesDivider();
+
+    const repliesUl = createElement("ul", {
+        children: [],
+        role: "none"
+    });
+
+    const repliesDiv = createElement("div", {
+        children: [repliesDivider, repliesUl],
+        className: "select-menu-list particular-replies"
+    });
+
+    for (const [i, reply] of savedReplies.entries()) {
         const button = createElement("button", {
             children: [
                 createElement("div", {
@@ -49,11 +122,27 @@ const createParticularRepliesDiv = (savedReplies) => {
                             ],
                             className: "flex-auto col-9",
                         }),
+                        createElement("div", {
+                            children: [
+                                createElement("code", {
+                                    children: [
+                                        createElement("span", {
+                                            children: [
+                                                `ctrl ${i}`
+                                            ],
+                                            className: "border rounded-2 p-1 no-wrap"
+                                        })
+                                    ]
+                                })
+                            ],
+                            className: "col-3 text-right"
+                        })
                     ],
                     className: "select-menu-item-text d-flex flex-items-center",
-                }),
+                })
             ],
             className: "select-menu-item width-full",
+            "data-ga-click": "Saved Replies, click, saved reply number 3",
             role: "menuitem",
             type: "button",
         });
@@ -63,14 +152,13 @@ const createParticularRepliesDiv = (savedReplies) => {
             event.preventDefault();
         });
 
-        var replyContainer = createElement("ul", {
-            children: [
-                createElement("li", { children: [button], role: "none" }),
-            ],
+        var replyButtonLi = createElement("li", {
+            children: [button],
             role: "none",
+            "data-value": reply.name
         });
 
-        repliesDiv.appendChild(replyContainer);
+        repliesUl.appendChild(replyButtonLi);
     }
 
     return repliesDiv;
