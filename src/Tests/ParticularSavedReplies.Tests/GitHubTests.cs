a6a271210e5a3f37063c4d8ba0dcd8fc3930c83c
Adam Wright <awright18@gmail.com>
2023-09-10 04:49:57 -0500

    Added Verification tests

--- src/Tests/ParticularSavedReplies.Tests/GitHubTests.cs
+++ src/Tests/ParticularSavedReplies.Tests/GitHubTests.cs
@@ -1,104 +1,57 @@
 ﻿namespace ParticularSavedReplies.Tests;
 
-using Microsoft.Extensions.Configuration;
 using Microsoft.Playwright;
-using Microsoft.Playwright.NUnit;
 using NUnit.Framework;
 using System.Threading.Tasks;
 
 [Parallelizable(ParallelScope.Self)]
 [TestFixture]
-public class GitHubTests : PageTest
+public class GitHubTests : ChromeExtensionPageTest
 {
-    public override BrowserNewContextOptions ContextOptions()
+    public GitHubTests()
     {
-        return new ()
-        {
-            StorageStatePath = ".auth/state.json"
-        };
+        PathToExtension = Path.GetFullPath(@".\ParticularSavedReplies");
     }
-    private IConfiguration _config;
     
-    public IConfiguration Configuration
-    {
-        get
-        {
-            if (_config == null)
-            {
-                var builder = new ConfigurationBuilder().AddJsonFile(".auth/credentials.json", optional: false);
-                _config = builder.Build();
-            }
-
-            return _config;
-        }
-    }
-
+    [SetUp]
     public async Task LoginToGitHub()
-    {
-        var gitHubLogin = Configuration.GetGitHubLogin();   
-         
-        // Create a Chromium browser instance
-        await Page.GotoAsync("https://github.com/login");
-        // Interact with login form
-        await Page.GetByLabel("Username or email address")
-            .FillAsync(gitHubLogin.UserName);
-        await Page.GetByLabel("Password")
-            .FillAsync(gitHubLogin.Password);
-        await Page.GetByRole(AriaRole.Button, new() { Name = "Sign in" })
-            .ClickAsync();
-        
-        await Page.Context.StorageStateAsync(new BrowserContextStorageStateOptions()
-        {
-            Path = ".auth/state.json"
-        });
-    }
-
-    [Test]
-    public async Task HomePage_Is_GitHub()
     {
         var gitHubLogin = Configuration.GetGitHubLogin();
 
-        var pathToExtension = Path.GetFullPath(@".\ParticularSavedReplies");
-        var browser = await Playwright.Chromium.LaunchPersistentContextAsync(
-            "",
-            new()
-            {
-                Headless = false,
-                Args = new[]
-                {
-                    $"--disable-extensions-except={pathToExtension}",
-                    $"--load-extension={pathToExtension}"
-                }
-            });
+        var gitHubLoginUrl = "https://github.com/login";
         
-        var page = await browser.NewPageAsync();
+        await Page.GotoAsync(gitHubLoginUrl);
         
-        page.Console += (console, msg) => Console.WriteLine(msg.Text);
-        
-        await page.GotoAsync("https://github.com/login");
-        // Interact with login form
-        await page.GetByLabel("Username or email address")
-            .FillAsync(gitHubLogin.UserName);
-        await page.GetByLabel("Password")
-            .FillAsync(gitHubLogin.Password);
-        await page.GetByRole(AriaRole.Button, new() { Name = "Sign in" })
-            .ClickAsync();
-        
-        await page.GotoAsync("https://github.com/particular/nservicebus/issues/1");
+        if (Page.Url == gitHubLoginUrl)
+        {
+            // Interact with login form
+            await Page.GetByLabel("Username or email address")
+                .FillAsync(gitHubLogin.UserName);
+            await Page.GetByLabel("Password")
+                .FillAsync(gitHubLogin.Password);
+            await Page.GetByRole(AriaRole.Button, new() { Name = "Sign in" })
+                .ClickAsync();
+        }
+    }
+    
+    [Test]
+    public async Task Can_add_particular_saved_replies_to_github_issue()
+    {
+        Page.Console += (console, msg) => Console.WriteLine(msg.Text);
 
-        await page.Locator("#new_comment_field").FocusAsync();
-        
-        await page.Keyboard.PressAsync("Control+.");
+        await Page.GotoAsync("https://github.com/particular/nservicebus/issues/1");
 
-        await page.Locator(".refresh-particular-replies").WaitForAsync(new()
+        await Page.Locator("#new_comment_field").FocusAsync();
+        
+        await Page.Keyboard.PressAsync("Control+.");
+
+        await Page.Locator(".refresh-particular-replies").WaitForAsync(new()
         {
             Timeout = 1000
         });
 
-        await page.ScreenshotAsync(new PageScreenshotOptions()
-        {
-            Path = "login.jpg",
-            FullPage = true
-        });
+        var replies = await Page.QuerySelectorAsync("div.select-menu-list.particular-replies");
+
+        await Verify(replies);
     }
 }
---
+++ src/Tests/ParticularSavedReplies.Tests/ModuleInitializer.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ParticularSavedReplies.Tests;
+
+public class ModuleInitializer
+{
+    [ModuleInitializer]
+    public static void InitPlaywright() =>
+        VerifyPlaywright.Initialize();
+
+    [ModuleInitializer]
+    public static void InitVerifier() => Verifier.UseProjectRelativeDirectory("approvals");
+    
+}
