8b30ede003d04ed2fcbec5324e382eed12fbcce3
Adam Wright <awright18@gmail.com>
2023-09-13 22:18:09 -0500

    fixing package.ps1

--- package.ps1
+++ package.ps1
@@ -6,8 +6,8 @@ $manifest = Get-Content $extensionPath/manifest.json | ConvertFrom-Json
 $manifest.version = $version
 $manifest | ConvertTo-Json -Depth 32 | Set-Content $extensionPath/manifest.json
 
-$versionedExtension = $extensionName-$version
-$extensionZip = $versionedExtension.zip
+$versionedExtension = "$extensionName-$version"
+$extensionZip = "$versionedExtension.zip"
 
 New-Item -ItemType Directory -Force "extension"
 Compress-Archive -Force -Path $extensionPath -Destination "./extension/$extensionZip"
